418660b118ef135a468c8650e6480a1e8b2bc64a
gbpark <gbpark@sypolaris.co.kr>
2026-03-08 18:01:06 +0900

    1. 모니터링 추가 2. 로그백 추가

--- src/main/java/com/spring_thief/my_resume/thymeleaf/controller/MainController.java
+++ src/main/java/com/spring_thief/my_resume/thymeleaf/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.spring_thief.my_resume.thymeleaf.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    private final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("/")
     public String root() {
+        log.info("일반 INFO 로그가 생성되었습니다.");
+        log.warn("경고 WARN 로그가 생성되었습니다.");
+        log.error("!!! 에러 ERROR 로그가 생성되었습니다 !!!");
         return "redirect:/resume";
     }
 
