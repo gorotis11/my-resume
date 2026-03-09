abd3ad930f3d4d7ef3dff7d3465f2d32b90dc790
gbpark <gbpark@sypolaris.co.kr>
2026-03-10 00:09:29 +0900

    1. 개발기 젠킨스 추가

---
+++ jenkins/dev-jenkinsfile.groovy
@@ -0,0 +1,57 @@
+pipeline {
+    agent {
+        kubernetes {
+            yaml """
+                apiVersion: v1
+                kind: Pod
+                spec:
+                  containers:
+                  - name: kaniko
+                    image: gcr.io/kaniko-project/executor:debug
+                    command:
+                    - sleep
+                    args:
+                    - 9999999
+                    volumeMounts:
+                    - name: registry-auth
+                      mountPath: /kaniko/.docker
+                  volumes:
+                  - name: registry-auth
+                    emptyDir: {}
+                """
+        }
+    }
+
+    environment {
+        // 아까 설치한 로컬 레지스트리 주소
+        REGISTRY = "local-registry.registry.svc.cluster.local:5000"
+        IMAGE_NAME = "my-resume"
+        TAG = "latest"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // GitHub에서 소스 코드 가져오기
+                checkout scm
+            }
+        }
+
+        stage('Build and Push') {
+            steps {
+                container('kaniko') {
+                    // Kaniko 실행: Docker 데몬 없이 이미지 빌드 및 푸시
+                    sh """
+                    /kaniko/executor \
+                    --context=\${WORKSPACE} \
+                    --dockerfile=Dockerfile \
+                    --destination=\${REGISTRY}/\${IMAGE_NAME}:\${TAG} \
+                    --insecure \
+                    --skip-tls-verify \
+                    --cache=true
+                    """
+                }
+            }
+        }
+    }
+}
